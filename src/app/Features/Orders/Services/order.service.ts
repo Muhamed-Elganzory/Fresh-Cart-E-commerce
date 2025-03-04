1d43162fe3ae920d707b128767b6c1050dc373a3
Muhamed-Elganzory <mohamedelganzory621@gmail.com>
2025-03-04 22:43:13 +0200

    test

--- src/app/Features/Orders/Services/order.service.ts
+++ src/app/Features/Orders/Services/order.service.ts
@@ -13,8 +13,8 @@ export class OrderService {
   // private readonly authService: AuthService = inject (AuthService);
 
   createCheckOut(cartID: string | null, shippingAddress:{details: string, phone: string, city: string}): Observable <any>{
-    // const cancel_url = '?url=http://localhost:4200/';
-    const returnURL = '?url=https://fresh-cart-e-commerce-nu.vercel.app';
+    // const cancel_url = '?url=http://localhost:4200/'; https://fresh-cart-e-commerce-nu.vercel.app/cart
+    const returnURL = '?url=https://fresh-cart-e-commerce-nu.vercel.app/cart';
     return this.httpClient.post(testingEnvironment.baseURL + 'orders/checkout-session/' + cartID + returnURL,
       {
         shippingAddress: shippingAddress,
