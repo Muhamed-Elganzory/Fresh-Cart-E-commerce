e99332bf7c1c294931534be9deca408872bcc738
Muhamed-Elganzory <mohamedelganzory621@gmail.com>
2025-03-04 22:35:43 +0200

    test

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,24 +2,7 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'product-details/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'checkout/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'login',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'register',
-    renderMode: RenderMode.Client,
-  },
-
-  {
     path: '**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   }
 ];
